ba22832c628a34acd5f257a97760a36bd84e206e
Trouble-Fete <neyroud.thomas@gmail.com>
2025-04-02 13:31:05 +0200

    region Image ok only Park didn't find

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,19 +1,19 @@
--- Création de la base de données (optionnel selon votre SGBD)
+-- Création de la base de données
 CREATE DATABASE IF NOT EXISTS verdansk_db;
 USE verdansk_db;
 
--- Création de la table des régions avec image représentative
+-- Création de la table des régions avec image des regions
 CREATE TABLE regions (
     region_id INT PRIMARY KEY AUTO_INCREMENT,
     region_name VARCHAR(100) NOT NULL,
-    region_image VARCHAR(255)  -- URL ou chemin de l'image
+    region_image VARCHAR(255)
 );
 
 -- Création de la table des armes avec image et type d'arme
 CREATE TABLE weapons (
     weapon_id INT PRIMARY KEY AUTO_INCREMENT,
     weapon_name VARCHAR(100) NOT NULL,
-    weapon_image VARCHAR(255),  -- URL ou chemin de l'image de l'arme
+    weapon_image VARCHAR(255),
     weapon_type ENUM('sniper', 'AR', 'smg', 'fusil tatic', 'pistolet', 'corps à corps', 'mitrailleuse', 'explosive') NOT NULL
 );
 
@@ -41,13 +41,13 @@ INSERT INTO regions (region_name, region_image) VALUES
   ('Lumber', 'https://lh5.googleusercontent.com/nNsisLgVQr4cpHFUgD_ZDepPlLjJnSovI4kBS1xOC34V34A627lsa3PSTt68r5k5gJZLDDcqDtMiBDAIWzFyXHBkV0YAlgjN7n4DB9gdPS4agIlA1EDL5hC5Gfzdo4TYu1BjkOCS'),
   ('Military Base', 'https://lh3.googleusercontent.com/OO43bJhGH4F5Mh5ssuvaG05OTW1gy3KxhblilNpQRYbrJ-RNGG4dsgyDjJCSQGdAD6AZW8n8xK3cjwm8mVhI9-dmWDt7-WcLnFHtEUWOrFOSu4gDEDqkz6OeSqvh1pOClvvWlDSL'),
   ('Train Station', 'https://example.com/images/verdansk_garrison.jpg'),
-  ('Boneyard', 'https://example.com/images/verdansk_garrison.jpg'),
+  ('Boneyard', 'https://callofdutymaps.com/wp-content/uploads/boneyard8-768x432.jpg'),
   ('SuperStore', 'https://chumley.barstoolsports.com/union/2022/08/14/EmFMB0hWoAI9aVs.196a0a0e.jpeg?canvas=4:3&bg-color=000000'),
   ('Prison', 'https://www.dexerto.fr/cdn-cgi/image/width=3840,quality=75,format=auto/https://editors.dexerto.fr/wp-content/uploads/sites/2/2023/12/09/retour-verdansk-warzone-semble-se-preciser.jpg'),
-  ('Promenade East', 'https://example.com/images/verdansk_garrison.jpg'),
-  ('Promenade West', 'https://example.com/images/verdansk_garrison.jpg'),
-  ('Hills', 'https://example.com/images/verdansk_garrison.jpg'),
-  ('Park', 'https://example.com/images/verdansk_garrison.jpg'),
+  ('Promenade East', 'https://th.bing.com/th/id/R.4d752a24abd6f07b262c1f7e545424f0?rik=UxBYoRHxvuhzEw&riu=http%3a%2f%2foyster.ignimgs.com%2fwordpress%2fstg.ign.com%2f2021%2f03%2fZONE3D-031B-Apartments-Block-North.jpg&ehk=CfnytT8NaWsCD2USeqtPFbEO653dkOt%2fo%2fh7Q%2ffFr48%3d&risl=&pid=ImgRaw&r=0'),
+  ('Promenade West', 'https://callofdutymaps.com/wp-content/uploads/warzonepromenadewest-740x416.jpg'),
+  ('Hills', 'https://wallpapercave.com/wp/wp8326840.jpg'),
+  ('Park', 'https://example.com/images/verdansk_garrison.jpg'), --pas trouvée encore.....
   ('Farmland', 'https://callofdutymaps.com/wp-content/uploads/verdansk843-1536x864.jpg');
 
 -- armes de Warzone
@@ -62,8 +62,9 @@ INSERT INTO weapons (weapon_name, weapon_image, weapon_type) VALUES
   ('725', 'https://example.com/images/725.jpg', 'fusil tatic'),
   ('1911', 'https://example.com/images/1911.jpg', 'pistolet'),
   ('M19', 'https://example.com/images/m19.jpg', 'pistolet'),
-  ('Combat Knife', 'https://example.com/images/knife.jpg', 'corps à corps'),
+  ('Couteau de combat', 'https://example.com/images/knife.jpg', 'corps à corps'),
   ('PKM', 'https://example.com/images/pkm.jpg', 'mitrailleuse'),
+  ('kAR 98', 'https://example.com/images/pkm.jpg', 'fusil tactique'),
   ('JOKR', 'https://example.com/images/jokr.jpg', 'explosive');
 
 -- exemples de rotations entre régions
